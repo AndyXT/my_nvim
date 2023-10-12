d9c4587177eef81a8aac5e79342ef473da74abf9
AndyXT <axtreto@me.com>
2023-10-12 00:03:59 -0400

    Named the which key maps

--- init.lua
+++ init.lua
@@ -1001,53 +1001,57 @@ require('copilot').setup({
 
 require("telescope").load_extension('harpoon')
 
-vim.keymap.set("n", "<leader>ha", function() require("harpoon.mark").add_file() end)
-vim.keymap.set("n", "<leader>hm", function() require("harpoon.ui").toggle_quick_menu() end)
+vim.keymap.set("n", "<leader>ha", "", { desc = 'Harpoon' })
+vim.keymap.set("n", "<leader>ha", function() require("harpoon.mark").add_file() end, { desc = 'Add File' })
+vim.keymap.set("n", "<leader>hm", function() require("harpoon.ui").toggle_quick_menu() end, { desc = 'Toggle Menu' })
 
 
 require('refactoring').setup({})
 
-vim.keymap.set("x", "<leader>re", function() require('refactoring').refactor('Extract Function') end)
-vim.keymap.set("x", "<leader>rf", function() require('refactoring').refactor('Extract Function To File') end)
+vim.keymap.set("x", "<leader>re", function() require('refactoring').refactor('Extract Function') end, { desc = 'Extract Function' })
+vim.keymap.set("x", "<leader>rf", function() require('refactoring').refactor('Extract Function To File') end, { desc = 'Extract Function to File' })
 -- Extract function supports only visual mode
-vim.keymap.set("x", "<leader>rv", function() require('refactoring').refactor('Extract Variable') end)
+vim.keymap.set("x", "<leader>rv", function() require('refactoring').refactor('Extract Variable') end, { desc = 'Extract Variable' })
 -- Extract variable supports only visual mode
-vim.keymap.set("n", "<leader>rI", function() require('refactoring').refactor('Inline Function') end)
+vim.keymap.set("n", "<leader>rI", function() require('refactoring').refactor('Inline Function') end, { desc = 'Inline Function' })
 -- Inline func supports only normal
-vim.keymap.set({ "n", "x" }, "<leader>ri", function() require('refactoring').refactor('Inline Variable') end)
+vim.keymap.set({ "n", "x" }, "<leader>ri", function() require('refactoring').refactor('Inline Variable') end, { desc = 'Inline Variable' })
 -- Inline var supports both normal and visual mode
 
-vim.keymap.set("n", "<leader>rb", function() require('refactoring').refactor('Extract Block') end)
-vim.keymap.set("n", "<leader>rbf", function() require('refactoring').refactor('Extract Block To File') end)
+vim.keymap.set("n", "<leader>rb", function() require('refactoring').refactor('Extract Block') end, { desc = 'Extract Block' })
+vim.keymap.set("n", "<leader>rbf", function() require('refactoring').refactor('Extract Block To File') end, { desc = 'Extract Block To File' })
 -- Extract block supports only normal mode
 
-vim.keymap.set(
-    {"n", "x"},
-    "<leader>rr",
-    function() require('refactoring').select_refactor() end
-)
+-- vim.keymap.set(
+--   {"n", "x"},
+--   "<leader>rr",
+--   function() require('refactoring').select_refactor() end,
+--   { desc = 'Select Refactor Menu' }
+-- )
 
 -- load refactoring Telescope extension
 require("telescope").load_extension("refactoring")
 
 vim.keymap.set(
-	{"n", "x"},
-	"<leader>rr",
-	function() require('telescope').extensions.refactoring.refactors() end
+  {"n", "x"},
+  "<leader>rr",
+  function() require('telescope').extensions.refactoring.refactors() end,
+  { desc = 'Select Refactor Menu' }
 )
 
 -- You can also use below = true here to to change the position of the printf
 -- statement (or set two remaps for either one). This remap must be made in normal mode.
 vim.keymap.set(
-	"n",
-	"<leader>rp",
-	function() require('refactoring').debug.printf({below = false}) end
+  "n",
+  "<leader>rp",
+  function() require('refactoring').debug.printf({below = false}) end,
+  { desc = 'Debug Printf' }
 )
 
 -- Print var
 
-vim.keymap.set({"x", "n"}, "<leader>rv", function() require('refactoring').debug.print_var() end)
+vim.keymap.set({"x", "n"}, "<leader>rv", function() require('refactoring').debug.print_var() end, { desc = 'Debug Print Var' })
 -- Supports both visual and normal mode
 
-vim.keymap.set("n", "<leader>rc", function() require('refactoring').debug.cleanup({}) end)
+vim.keymap.set("n", "<leader>rc", function() require('refactoring').debug.cleanup({}) end, { desc = 'Debug Cleanup' })
 -- Supports only normal mode
