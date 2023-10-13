7248138b97df3a8b03da2974c16c646839e2026f
AndyXT <30579562+AndyXT@users.noreply.github.com>
2023-10-13 09:10:08 -0400

    Update init.lua

--- init.lua
+++ init.lua
@@ -721,6 +721,9 @@ end
 -- [[ Configure nvim-cmp ]]
 -- See `:help cmp`
 local cmp = require("cmp")
+local luasnip = require 'luasnip'
+require('luasnip.loaders.from_vscode').lazy_load()
+luasnip.config.setup {}
 local lspkind = require('lspkind')
 
 local has_words_before = function()
